7b9ea708409bd9ee7e07f9a0dff639e1debd481e
kwakminkyeong <minky_kwak@naver.com>
2023-11-08 14:02:20 +0900

    upload

---
+++ QT/js/script.js
@@ -0,0 +1,11 @@
+let menu=document.querySelector(".menu");
+let sub=document.querySelector(".submenu");
+function showMenu(){
+    submenu.style
+}
+
+
+
+
+
+let gonggi=document.querySelector(".gonggi");
